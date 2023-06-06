b0dfeb3b9b94ad9f0d526235eba76851a5a73755
BilinskijDima <bilinskijwork@gmail.com>
2023-06-06 20:08:35 +0300

    Initial commit

---
+++ Trektronic-App/Trektronic-App/Trektronic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trektronic_AppApp.swift
+//  Trektronic-App
+//
+//  Created by Дмитрий Билинский on 6.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Trektronic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
