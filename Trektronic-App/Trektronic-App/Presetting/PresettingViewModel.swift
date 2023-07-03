ed7a6127bb4883317933590024f5f25b3f1b0e65
BilinskijDima <bilinskijwork@gmail.com>
2023-07-03 14:01:35 +0300

    add fix file

---
+++ Trektronic-App/Trektronic-App/Presetting/PresettingViewModel.swift
@@ -0,0 +1,111 @@
+//
+//  PresettingViewModel.swift
+//  Trektronic-App
+//
+//  Created by Дмитрий Билинский on 24.06.23.
+//
+
+import Foundation
+import SwiftUI
+
+final class PresettingViewModel: ObservableObject  {
+    
+    private var healthKitManager: HealthKitManagerProtocol = HealthKitManager()
+    private var fireBaseManager: FirebaseManagerProtocol = FirebaseManager()
+    
+    @AppStorage("stateLoadingView") var stateLoadView: LoadView = .loginView
+    @AppStorage("userID") var userID = DefaultSettings.userID
+    
+    @Published var showImagePicker = false
+    @Published var image: UIImage?
+    @Published var imageURL: String?
+    
+    @Published var textFieldName = ""
+    @Published var disableTextFieldName = false
+    @Published var stateNickname = false
+    
+    @Published var stateHealthKit = false
+    
+    @Published var errorText = ""
+    @Published var setData = false
+    
+    @MainActor
+    func requestAuthorisation() {
+        Task {
+            stateHealthKit = try await healthKitManager.requestAuthorisation()
+        }
+    }
+    
+    func stateSaveData() {
+        if self.setData {
+            self.setDataRealTime()
+        } else {
+            self.stateLoadView = .tabBarView
+        }
+    }
+    
+    func checkNickname() {
+        guard !self.textFieldName.isEmpty else {
+            self.errorText = "вы не ввели имя"
+            self.stateNickname = false
+            return}
+        
+        fireBaseManager.checkUserNameAlreadyExist(newUserName: self.textFieldName) { [weak self] result in
+            guard let self = self else {return}
+            if result {
+                self.errorText = "извините это имя уже занято"
+                self.stateNickname = false
+            } else {
+                self.stateNickname = true
+                self.errorText = ""
+            }
+        }
+    }
+    
+    func setDataRealTime() {
+        Task {
+          
+            guard let defaultImage = UIImage(systemName: "person.circle.fill") else {return}
+            
+            let imageURL = try await fireBaseManager.persistImageToStorage(userID: userID, image: image ?? defaultImage)
+
+            let user = Users(date: Date.now.description, step: 0, coin: 0, nickname: textFieldName, image: imageURL, favouritesUser: [""], id: userID)
+        
+            try await fireBaseManager.setDataRealTime(user: user, id: userID)
+            await MainActor.run {
+                self.stateLoadView = .tabBarView
+            }
+        }
+   
+    }
+
+    func stateAutUser() {
+        
+        self.fireBaseManager.getDataRealTime(id: userID) {[weak self] dataSnapshot in
+            guard let self = self else {return}
+            print(dataSnapshot.value!)
+           
+            if "\(dataSnapshot.value!)" == "<null>" {
+                print ("пользователь нет")
+                self.setData = true
+            } else {
+                do {
+                    let user = try dataSnapshot.decodeJSON(type: Users.self)
+                    print ("пользователь есть")
+                    self.imageURL = user.image
+                    self.textFieldName = user.nickname
+                    self.disableTextFieldName = true
+                    self.stateNickname = true
+                } catch {
+                    print (error.localizedDescription)
+                }
+            }
+            
+           
+            
+        }
+        
+    }
+    
+}
+
